0147575aeebfb1dd78c4e507aa057ea960fa8593
Inaki Ordonez-Etxeberria <iordonez002@ehu.es>
2015-07-15 11:12:45 +0100

    Update and rename whereistitan.py to WhereIsTitan.py

--- WhereIsTitan.py
+++ WhereIsTitan.py
@@ -2,7 +2,7 @@
 """
 Created on Mon Jun  8 09:57:38 2015
 
-@author: inaki
+@author: Inaki Ordonez-Etxeberria
 
 Script to determine the position of Titan in order to help the observation to the IDS Proposal SI2015a02 
 """
